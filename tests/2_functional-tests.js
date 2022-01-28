eef72f8bb14b8ab8700ab3f867e65c15cc7ac62b
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-28 12:33:13 +0100

    TEST create an issue with only required fields

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -20,7 +20,6 @@ suite("Functional Tests", function () {
         created_by: "test-created-by",
         assigned_to: "test-assigned-to",
         status_text: "test-status-text",
-        open: false,
       })
       .end((err, res) => {
         assert.equal(res.status, 200);
@@ -37,9 +36,32 @@ suite("Functional Tests", function () {
         });
       });
   });
-  // test("Create an issue with only required fields: POST request to `/api/issues/{project}`", (done) => {
-  //   assert.fail();
-  // });
+  test("Create an issue with only required fields: POST request to `/api/issues/{project}`", (done) => {
+    const now = new Date().getTime();
+    chai
+      .request(server)
+      .post(`/api/issues/fcc-${now}`)
+      .type("form")
+      .send({
+        issue_title: "test-issue-title",
+        issue_text: "test-issue-text",
+        created_by: "test-created-by",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        // search for an issue on project <now>, check it for 200 and deepequal
+        Issue.findOne({ project: `fcc-${now}` }, (err, obj) => {
+          if (err) console.error(err);
+          assert.equal(obj.issue_title, "test-issue-title");
+          assert.equal(obj.issue_text, "test-issue-text");
+          assert.equal(obj.created_by, "test-created-by");
+          assert.equal(obj.assigned_to, "");
+          assert.equal(obj.status_text, "");
+          assert.equal(obj.project, `fcc-${now}`);
+          done();
+        });
+      });
+  });
   // test("Create an issue with missing required fields: POST request to `/api/issues/{project}`", (done) => {
   //   assert.fail();
   // });
